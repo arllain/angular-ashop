547cc3f0983a4b3897c55233bfba223c13ce60e7
arllain <arllain@gmail.com>
2018-12-08 21:27:19 -0300

    new evironment config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  firebase:{
-    apiKey: "AIzaSyC7AaP0P4GWkAoOV-u9mLoDWoeV7v2E-Vg",
-    authDomain: "ashop-48ae7.firebaseapp.com",
-    databaseURL: "https://ashop-48ae7.firebaseio.com",
-    projectId: "ashop-48ae7",
-    storageBucket: "ashop-48ae7.appspot.com",
-    messagingSenderId: "391968793226"
+  firebase: {
+    apiKey: 'AIzaSyC-1CsHiwMf3JWEWscDGRbkdo4I3ljKpJk',
+    authDomain: 'ashop-dab0d.firebaseapp.com',
+    databaseURL: 'https://ashop-dab0d.firebaseio.com',
+    projectId: 'ashop-dab0d',
+    storageBucket: '',
+    messagingSenderId: '424061726410'
   }
 };
